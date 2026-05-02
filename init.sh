aa835d617244e9eba967b3c09aa8f5cb8176467c
Pontus Berlin <pontus@b3rl1n.com>
2026-05-02 15:20:03 +0200

    Removed obsolete setup steps

--- init.sh
+++ init.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 if [ ! -f .env ]; then
 	cp ./files/.env.example .env
+  chmod 600 .env
 fi
-if [ ! -f ./secrets/admin_password.secret ]; then
-	touch ./secrets/admin_password.secret
-fi
-if [ ! -f ./secrets/db_password.secret ]; then
-	touch ./secrets/db_password.secret
-fi
-if [ ! -f ./secrets/email_password.secret ]; then
-	touch ./secrets/email_password.secret
-fi
-if [ ! -f ./secrets/secret_key.secret ]; then
-	touch ./secrets/secret_key.secret
-fi
-mkdir -p ./data/consume/
